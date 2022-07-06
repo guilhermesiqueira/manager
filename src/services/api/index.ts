2a7a9c615e73c68ccaaa4d90bc3c0b0059bba2d6
Nathiely Macedo <nathielymm@gmail.com>
2022-07-06 10:23:43 -0300

    feat: add global style to app.tsx

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -5,7 +5,7 @@ import snakeCaseKeys from "snakecase-keys";
 const RIBON_API = "http://dev-api.eba-fktmq9bg.us-east-1.elasticbeanstalk.com/";
 
 export const baseURL = process.env.REACT_APP_RIBON_API || RIBON_API;
-export const API_SCOPE = "/api/admin";
+export const API_SCOPE = "/api/v1";
 
 const api = Axios.create({
   baseURL,
