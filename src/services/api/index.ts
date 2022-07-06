ac0e9cdb5308a6abaae13828b45370950875ba6a
Nathiely Macedo <nathielymm@gmail.com>
2022-07-06 08:38:35 -0300

    feat: add service api

---
+++ src/services/api/index.ts
@@ -0,0 +1,63 @@
+import Axios, { AxiosRequestConfig } from "axios";
+import camelCaseKeys from "camelcase-keys";
+import snakeCaseKeys from "snakecase-keys";
+
+const RIBON_API = "http://dev-api.eba-fktmq9bg.us-east-1.elasticbeanstalk.com/";
+
+export const baseURL = process.env.REACT_APP_RIBON_API || RIBON_API;
+export const API_SCOPE = "/api/admin";
+
+const api = Axios.create({
+  baseURL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  validateStatus: (status) => status >= 200 && status < 300,
+});
+
+api.interceptors.request.use((request) =>
+  request?.data
+    ? { ...request, data: snakeCaseKeys(request?.data, { deep: true }) }
+    : request,
+);
+
+api.interceptors.response.use(
+  (response) => ({
+    ...response,
+    data: camelCaseKeys(response.data, { deep: true }),
+  }),
+  (error) => Promise.reject(error),
+);
+
+api.interceptors.request.use((config) => {
+  // eslint-disable-next-line no-param-reassign
+  config.headers = { ...config.headers };
+
+  return config;
+});
+
+export function apiGet(url: string, config?: AxiosRequestConfig) {
+  if (config) return api.get(`${API_SCOPE}/${url}`, config);
+
+  return api.get(`${API_SCOPE}/${url}`);
+}
+
+export function apiPost(url: string, data: any, config?: AxiosRequestConfig) {
+  if (config) return api.post(`${API_SCOPE}/${url}`, data, config);
+
+  return api.post(`${API_SCOPE}/${url}`, data);
+}
+
+export function apiPut(url: string, data: any, config?: AxiosRequestConfig) {
+  if (config) return api.put(`${API_SCOPE}/${url}`, data, config);
+
+  return api.put(`${API_SCOPE}/${url}`, data);
+}
+
+export function apiDelete(url: string, config?: AxiosRequestConfig) {
+  if (config) return api.delete(`${API_SCOPE}/${url}`, config);
+
+  return api.delete(`${API_SCOPE}/${url}`);
+}
+
+export default api;
