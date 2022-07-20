2212696c3fef726aa3f3e82c4f310118b30bcb21
Clara Mendes <claraplanejamento@gmail.com>
2022-07-20 17:41:12 -0300

    feat: adds dyn. mapping of integrations into cards

--- src/presentation/components/atomics/Cards/IntegrationCard/index.tsx
+++ src/presentation/components/atomics/Cards/IntegrationCard/index.tsx
@@ -1,11 +1,17 @@
 import { Box, Text } from "@chakra-ui/react";
 
-function IntegrationCard() {
+type Props = {
+  title: string;
+  subtitle: string;
+  value: string;
+}
+
+function IntegrationCard({ title, subtitle, value }: Props) {
   return (
     <Box height='97px' width='176px' padding='16px' border='1px' borderColor='gray' borderRadius='16px'>
-      <Text textColor='#185669' fontWeight={700} fontSize='12px'>Integration Name</Text>
-      <Text textColor='#185669' fontWeight={300} fontSize='10px'>Assigned (USDC)</Text>
-      <Text textColor='#00CDB4' fontWeight={700} fontSize='16px'>351.895,12</Text>
+      <Text textColor='#185669' fontWeight={700} fontSize='12px'>{title}</Text>
+      <Text textColor='#185669' fontWeight={300} fontSize='10px'>{subtitle}</Text>
+      <Text textColor='#00CDB4' fontWeight={700} fontSize='16px'>{value}</Text>
     </Box>
   )
 }
--- src/presentation/pages/Dashboard/index.tsx
+++ src/presentation/pages/Dashboard/index.tsx
@@ -1,17 +1,25 @@
 import { SimpleGrid } from "@chakra-ui/react";
 import IntegrationCard from "presentation/components/atomics/Cards/IntegrationCard";
 
+const integrationsArray = [{ id: 1, name: "Dobra", type: "Assigned (USDC)", value: "128.412,00" },
+{ id: 2, name: "ME poupe", type: "Assigned (USDC)", value: "999.999,00" },
+{ id: 3, name: "Dobra", type: "Assigned (USDC)", value: "128.412,00" },
+{ id: 4, name: "ME poupe", type: "Assigned (USDC)", value: "999.999,00" },
+{ id: 5, name: "Dobra", type: "Assigned (USDC)", value: "128.412,00" },
+{ id: 6, name: "ME poupe", type: "Assigned (USDC)", value: "999.999,00" }]
+
 function DashboardPage(): JSX.Element {
   return (
     <>
       <h1>dashboard</h1>
       <SimpleGrid fontFamily='Inter' columns={4} gap='8px' bgColor='white' >
-        <IntegrationCard />
-        <IntegrationCard />
-        <IntegrationCard />
-        <IntegrationCard />
-        <IntegrationCard />
-        <IntegrationCard />
+        {integrationsArray.map(integration => (
+          <IntegrationCard
+            key={integration.id}
+            title={integration.name}
+            subtitle={integration.type}
+            value={integration.value} />
+        ))}
       </SimpleGrid>
     </>
   )
