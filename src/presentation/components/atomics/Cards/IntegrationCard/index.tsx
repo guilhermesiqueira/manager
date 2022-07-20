83db2e643cf0ea5b82a9036ca96d7766a20bd33b
Clara Mendes <claraplanejamento@gmail.com>
2022-07-20 18:23:48 -0300

    feat: adds sorting by value

--- src/presentation/components/atomics/Cards/IntegrationCard/index.tsx
+++ src/presentation/components/atomics/Cards/IntegrationCard/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Text } from "@chakra-ui/react";
+import { GridItem, Text } from "@chakra-ui/react";
 
 type Props = {
   title: string;
@@ -8,11 +8,11 @@ type Props = {
 
 function IntegrationCard({ title, subtitle, value }: Props) {
   return (
-    <Box height='97px' width='176px' padding='16px' border='1px' borderColor='gray' borderRadius='16px'>
+    <GridItem height="97px" padding='16px' border='1px' borderColor='gray' borderRadius='16px'>
       <Text textColor='#185669' fontWeight={700} fontSize='12px'>{title}</Text>
       <Text textColor='#185669' fontWeight={300} fontSize='10px'>{subtitle}</Text>
       <Text textColor='#00CDB4' fontWeight={700} fontSize='16px'>{value}</Text>
-    </Box>
+    </GridItem>
   )
 }
 
