9e6a3aebc9974c47fdb26bfa4ce8718208da9e90
Clara Mendes <claraplanejamento@gmail.com>
2022-07-20 18:51:01 -0300

    style: fixes scrollbar and spacing

--- src/presentation/pages/Dashboard/index.tsx
+++ src/presentation/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import { Grid, Heading } from "@chakra-ui/react";
+import { Box, Grid, Heading } from "@chakra-ui/react";
 import IntegrationCard from "presentation/components/atomics/Cards/IntegrationCard";
 
 const integrationsArray = [{ id: 1, name: "Dobra", type: "Assigned (USDC)", value: "128.412,00" },
@@ -6,16 +6,17 @@ const integrationsArray = [{ id: 1, name: "Dobra", type: "Assigned (USDC)", valu
 { id: 3, name: "Dobra", type: "Assigned (USDC)", value: "228.412,00" },
 { id: 4, name: "Dinheiro na nota", type: "Assigned (USDC)", value: "99.999,00" },
 { id: 5, name: "Dobra", type: "Assigned (USDC)", value: "128.412,00" },
-{ id: 6, name: "ME poupe", type: "Assigned (USDC)", value: "999.999,00" }]
+{ id: 6, name: "ME poupe", type: "Assigned (USDC)", value: "999.999,00" },
+]
 
 function DashboardPage(): JSX.Element {
   function removeDotComma(str: string) {
     return str.replaceAll(",", "").replaceAll(".", "")
   }
   return (
-    <>
-      <Heading fontFamily="Inter" fontSize="4xl" display="block">Treasure Dashboard</Heading>
-      <Grid maxH="500px" overflowY="scroll" fontFamily="Inter" templateColumns="repeat(4, 1fr)" gridAutoRows="max-content" gap="8px" bgColor='white'>
+    <Box display="flex" flexDir="column">
+      <Heading margin="84px" fontFamily="Inter" fontSize="4xl" display="block">Treasure Dashboard</Heading>
+      <Grid maxH="600px" w="100%" marginLeft="8px" overflowY="auto" fontFamily="Inter" templateColumns="repeat(4, 1fr)" gridAutoRows="max-content" gap="8px" bgColor='white'>
         {integrationsArray
           .sort((a, b) => parseInt(removeDotComma(b.value), 10) - parseInt(removeDotComma(a.value), 10))
           .map(integration => (
@@ -26,7 +27,7 @@ function DashboardPage(): JSX.Element {
               value={integration.value} />
           ))}
       </Grid>
-    </>
+    </Box>
   )
 }
 export default DashboardPage;
