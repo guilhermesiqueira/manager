0944a9eb90ffe61cff3a4cedc45ff4469f8ee76d
Henrique Barbosa <henrikleod@gmail.com>
2022-07-21 19:01:14 -0300

    feature: copyable table cell component

---
+++ src/components/atomics/CopyableTableCell/index.tsx
@@ -0,0 +1,17 @@
+import copyIcon from "./assets/copy-icon.svg";
+import * as S from "./styles";
+
+export type Props = {
+  text: string;
+};
+
+function CopyableTableCell({text}: Props): JSX.Element {
+  return (
+    <S.Container>
+      <img src={copyIcon} alt="copy to clipboard"/>
+      <span>{text}</span>
+    </S.Container>
+  );
+}
+
+export default CopyableTableCell;
