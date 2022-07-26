b6d39e9a5a0e8cff41f0be345d9e773a70d86c53
Clara Mendes <claraplanejamento@gmail.com>
2022-07-26 12:12:41 -0300

    feat: adds integration informations

--- src/pages/integrations/IntegrationDetailsPage/index.tsx
+++ src/pages/integrations/IntegrationDetailsPage/index.tsx
@@ -1,5 +1,6 @@
 import IntegrationCard from "components/moleculars/cards/IntegrationCard";
 import useApiIntegrations from "hooks/apiHooks/useApiIntegrations";
+import dateFormatter from "lib/dateFormatter";
 import { useCallback, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useParams } from "react-router";
@@ -30,15 +31,30 @@ function IntegrationDetailsPage(): JSX.Element {
   }, []);
 
   const integrationName = integration?.name;
-
+  const {status, name, walletAddress, integrationAddress, createdAt, updatedAt} = integration;
   useEffect(() => {
     fetchIntegration();
+    console.log(integration)
   }, []);
 
   return (
     <S.Container>
       <S.Title>{t("title", { integrationName })}</S.Title>
       <IntegrationCard title={integrationName} value="12" warning/>
+      <S.InfoName>{t("status")}</S.InfoName>
+      <S.InfoValue>{status}</S.InfoValue>
+      <S.InfoName>{t("id")}</S.InfoName>
+      <S.InfoValue>{id}</S.InfoValue>
+      <S.InfoName>{t("name")}</S.InfoName>
+      <S.InfoValue>{name}</S.InfoValue>
+      <S.InfoName>{t("walletAddress")}</S.InfoName>
+      <S.InfoValue>{walletAddress}</S.InfoValue>
+      <S.InfoName>{t("integrationAddress")}</S.InfoName>
+      <S.InfoValue>{integrationAddress}</S.InfoValue>
+      <S.InfoName>{t("createdAt")}</S.InfoName>
+      <S.InfoValue>{dateFormatter(createdAt)}</S.InfoValue>
+      <S.InfoName>{t("lastEditedAt")}</S.InfoName>
+      <S.InfoValue>{dateFormatter(updatedAt)}</S.InfoValue>
     </S.Container>
   );
 }
--- src/pages/integrations/IntegrationDetailsPage/styles.ts
+++ src/pages/integrations/IntegrationDetailsPage/styles.ts
@@ -16,3 +16,20 @@ export const Title = styled.h1`
     margin-top: 84px;
   `}
 `;
+
+export const InfoName = styled.p`
+  ${() => css`
+    margin-top: 16px;
+    font-weight: 600;
+    font-size: 12px;
+    font-family: "Inter";
+  `}
+`;
+
+export const InfoValue = styled.p`
+  ${() => css`
+    margin: 4px 0;
+    font-size: 12px;
+    font-family: "Inter";
+  `}
+`;
