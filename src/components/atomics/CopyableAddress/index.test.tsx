a8bbf5465b964a97ca68e01838fa98ca4526b24a
Henrique Barbosa <henrikleod@gmail.com>
2022-07-26 10:21:50 -0300

    feature: copyable address tests

---
+++ src/components/atomics/CopyableAddress/index.test.tsx
@@ -0,0 +1,26 @@
+import { screen } from "@testing-library/react";
+import { clickOn, renderComponent } from "config/testUtils";
+import CopyableAddress from ".";
+
+describe("CopyableAddress", () => {
+  const URL = "https://ribon.io";
+
+  it("should render without error", () => {
+    renderComponent(<CopyableAddress text={URL}/>);
+
+    expect(screen.getByText(URL)).toBeInTheDocument();
+  });
+
+  it("should copy to clipboard", () => {
+    Object.assign(window.navigator, {
+      clipboard: {
+        writeText: jest.fn().mockImplementation(() => Promise.resolve()),
+      },
+    });
+
+    renderComponent(<CopyableAddress text="click"/>);
+    clickOn("click");
+
+    expect(navigator.clipboard.writeText).toHaveBeenCalledWith("click");
+  });
+});
