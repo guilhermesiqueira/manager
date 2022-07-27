ed9f75d775c707ffc1a34f588521455e26c9f399
Clara Mendes <claraplanejamento@gmail.com>
2022-07-27 16:24:14 -0300

    refactor: renames describe and it date formatter

--- src/lib/dateFormatter/index.test.tsx
+++ src/lib/dateFormatter/index.test.tsx
@@ -1,8 +1,8 @@
 import dateFormatter from ".";
 
-describe("should date accordingly", () => {
+describe("#dateFormatter", () => {
   const acceptableDates = ["20/06/2022", "6/20/2022"]
-  it("to receive new date", () => {
+  it("returns correct date", () => {
     expect(dateFormatter("2022-06-20 17:55:49 UTC")).toBe(acceptableDates[0] || acceptableDates[1]); 
   });
 });
