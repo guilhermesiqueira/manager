e4df59feb0f8c70b74258653cc07bf374191b067
Clara Mendes <claraplanejamento@gmail.com>
2022-07-27 16:48:39 -0300

    fix: fixes prettier

--- src/lib/dateFormatter/index.test.tsx
+++ src/lib/dateFormatter/index.test.tsx
@@ -2,6 +2,6 @@ import dateFormatter from ".";
 
 describe("#dateFormatter", () => {
   it("returns correct date", () => {
-    expect(dateFormatter("2022-06-20 17:55:49 UTC")).toBe("6/20/2022"); 
+    expect(dateFormatter("2022-06-20 17:55:49 UTC")).toBe("6/20/2022");
   });
 });
