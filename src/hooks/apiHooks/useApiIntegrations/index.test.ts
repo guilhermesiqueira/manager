0ecd4d6a506e22e57c112b6fe564e475c33fd2f2
Clara Mendes <claraplanejamento@gmail.com>
2022-08-02 12:46:45 -0300

    test: adds updateApiIntegration test

--- src/hooks/apiHooks/useApiIntegrations/index.test.ts
+++ src/hooks/apiHooks/useApiIntegrations/index.test.ts
@@ -35,4 +35,29 @@ describe("useIntegrations", () => {
       expect(integrationsApi.getIntegration).toHaveBeenCalledWith(id);
     });
   });
+
+  describe("#updateApiIntegration", () => {
+    const id = 1;
+    const data = {
+      id: 1,
+      name: "Integration 1",
+      walletAddress: "0x1234567890123456789012345678901234567890",
+      url: "https://integration.com/1",
+      logo: "https://integration.com/1/logo.png",
+      integrationAddress: "0x1234567890123456789012345678901234567890",
+      status: "active",
+      created_at: "2020-01-01T00:00:00.000Z",
+      updated_at: "2020-01-01T00:00:00.000Z",
+    };
+    beforeEach(() => {
+      integrationsApi.updateIntegration = jest.fn(() => ({} as any));
+    });
+
+    it("calls updateApiIntegration with correct params", () => {
+      hook.updateApiIntegration(id, data);
+
+      expect(integrationsApi.updateIntegration).toHaveBeenCalled();
+      expect(integrationsApi.updateIntegration).toHaveBeenCalledWith(id, data);
+    });
+  });
 });
