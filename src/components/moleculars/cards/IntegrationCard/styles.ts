8cfa5434d2e397031992b399e9ea10bc3a2973f8
Clara Mendes <claraplanejamento@gmail.com>
2022-08-08 15:06:14 -0300

    feat: adds tooltip to integration card

---
+++ src/components/moleculars/cards/IntegrationCard/styles.ts
@@ -0,0 +1,14 @@
+import styled, { css } from "styled-components";
+
+export const Text = styled.div`
+  ${({ color, theme }) => css`
+    width : 100%;
+    background-color: ${color};
+    font-weight: ${theme.font.bold};
+    color: ${theme.colors.ribonWhite};
+    padding: 12px 16px;
+    border-radius: 8px;
+    font-size: 10px;
+    margin: 4px;
+  `}
+`
