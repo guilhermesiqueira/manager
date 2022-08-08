e050ac92cdeb87490865c399315465f9168b798c
Henrique Barbosa <henrikleod@gmail.com>
2022-08-08 11:17:31 -0300

    feature: button to add a new integration

--- src/pages/integrations/IntegrationsPage/index.tsx
+++ src/pages/integrations/IntegrationsPage/index.tsx
@@ -1,4 +1,7 @@
 import { useTranslation } from "react-i18next";
+import { Button } from "@chakra-ui/react";
+import theme from "styles/theme";
+import { useNavigate } from "react-router";
 import IntegrationsListSection from "./IntegrationsListSection";
 import * as S from "./styles";
 
@@ -7,9 +10,25 @@ function IntegrationsPage(): JSX.Element {
     keyPrefix: "integrations.integrationsList",
   });
 
+  const { bgGray, ribonBlack } = theme.colors;
+  
+  const navigate = useNavigate();
+
+  const handleAddNew = () => {
+    navigate("/integrations/new");
+  }
+
   return (
     <S.Container>
       <S.Title>{t("title")}</S.Title>
+        <Button
+          color={bgGray}
+          backgroundColor={ribonBlack}
+          marginLeft="8px"
+          onClick={handleAddNew}
+        >
+          {t("createNew")}
+        </Button>
       <IntegrationsListSection />
     </S.Container>
   );
