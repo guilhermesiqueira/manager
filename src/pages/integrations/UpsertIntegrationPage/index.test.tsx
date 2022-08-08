2f901058d7ec901358cadde8574c20f54561652a
Henrique Barbosa <henrikleod@gmail.com>
2022-08-08 14:54:41 -0300

    fix: lint issues

--- src/pages/integrations/UpsertIntegrationPage/index.test.tsx
+++ src/pages/integrations/UpsertIntegrationPage/index.test.tsx
@@ -5,14 +5,14 @@ import UpsertIntegrationPage from ".";
 
 describe("UpsertIntegrationPage", () => {
   it("should render the create page without error", () => {
-    renderComponent(<UpsertIntegrationPage/>);
+    renderComponent(<UpsertIntegrationPage />);
 
     expect(screen.getByText("Add New Integration")).toBeInTheDocument();
     expect(screen.getByText("Save")).toBeInTheDocument();
   });
 
   it("should render the edit page without error", () => {
-    renderComponent(<UpsertIntegrationPage isEdit/>);
+    renderComponent(<UpsertIntegrationPage isEdit />);
 
     expect(screen.getByText("Edit Integration")).toBeInTheDocument();
     expect(screen.getByText("Save changes")).toBeInTheDocument();
