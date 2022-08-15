b41cb980e6b764aa0f8bd9c5aaaeea2bfa69a619
Henrique Barbosa <henrikleod@gmail.com>
2022-08-15 11:08:44 -0300

    feature: integration type with optional params

--- src/types/entities/Integration.ts
+++ src/types/entities/Integration.ts
@@ -1,12 +1,13 @@
 import IntegrationWallet from "./IntegrationWallet";
 
 export default interface Integration {
-  id: number;
+  id?: number;
   name: string;
   integrationWallet?: IntegrationWallet;
-  integrationAddress: string;
-  uniqueAddress: string;
+  integrationAddress?: string;
+  uniqueAddress?: string;
   status: string;
+  ticketAvailabilityInMinutes: number | null;
   created_at?: string;
   updated_at?: string;
 }
