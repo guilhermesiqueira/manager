5927dfb6a17fa9002cccc78ff836b4688c2b08fe
let <let@MacBook-Air-de-let.local>
2022-09-09 13:18:58 -0300

    feat: transfer some envs to constants and adjust pipeline

---
+++ src/utils/constants.ts
@@ -0,0 +1,5 @@
+export const RIBON_API =
+  "http://ribon-core-api-dev.us-east-1.elasticbeanstalk.com/";
+
+export const THE_GRAPH_API =
+  "https://api.thegraph.com/subgraphs/name/ribondao/subgraphribon";
