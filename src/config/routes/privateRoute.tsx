810587fdc9dc659ee7a8dfdae16544c7c78d3b2b
Yan Galli <yangalli@hotmail.com>
2022-09-26 14:46:23 -0300

    feat: add token in private route

--- src/config/routes/privateRoute.tsx
+++ src/config/routes/privateRoute.tsx
@@ -1,7 +1,8 @@
 import { Navigate } from "react-router-dom";
+import { TOKEN_KEY } from "utils/constants";
 
 function PrivateRoute({ children }: any) {
-  const accessToken = localStorage.getItem("token");
+  const accessToken = localStorage.getItem(TOKEN_KEY);
 
   return accessToken ? children : <Navigate to="/" />;
 }
