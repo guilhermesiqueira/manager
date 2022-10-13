2b140b588bfdc6f6b1855b827032906b38a976fa
Henrique Barbosa <henrikleod@gmail.com>
2022-10-13 17:34:27 -0300

    fix: chart display

--- src/pages/DashboardPage/TreasureSection/styles.ts
+++ src/pages/DashboardPage/TreasureSection/styles.ts
@@ -1,15 +1,12 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  display: flex;
+  display: block;
 `;
 
 export const Card = styled.div`
-  display: flex;
-  flex-flow: row wrap;
   width: 256px;
   box-sizing: border-box;
-  position: absolute;
   background: ${({ theme }) => theme.colors.white};
   border: 1px solid ${({ theme }) => theme.colors.mediumGray};
   border-radius: 16px;
