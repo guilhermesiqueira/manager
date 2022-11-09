54fbad46117e3d732c553c7c782b709c0f6ff8d4
let <let@MacBook-Air-de-let.local>
2022-11-09 17:15:24 -0300

    feat: add tests

---
+++ src/services/api/offersApi/index.test.ts
@@ -0,0 +1,18 @@
+import offersApi from ".";
+import api from "..";
+
+describe("offersApi", () => {
+  describe("#getOffersList", () => {
+    beforeEach(() => {
+      api.get = jest.fn();
+    });
+
+    it("expects to send a get request with the correct info: url, params and headers", () => {
+      offersApi.getOffersList({ perPage: 15, page: 0 });
+
+      expect(api.get).toHaveBeenCalledWith("/api/v1/givings/offers", {
+        params: { params: { page: 0, per_page: 15 } },
+      });
+    });
+  });
+});
