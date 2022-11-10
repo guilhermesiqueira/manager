547e61dddd9d5531f093b1c3e28637fc2ec80743
let <let@MacBook-Air-de-let.local>
2022-11-10 13:55:44 -0300

    fix: test

--- src/services/api/offersApi/index.test.ts
+++ src/services/api/offersApi/index.test.ts
@@ -10,7 +10,7 @@ describe("offersApi", () => {
     it("expects to send a get request with the correct info: url, params and headers", () => {
       offersApi.getOffersList({ perPage: 15, page: 0 });
 
-      expect(api.get).toHaveBeenCalledWith("/api/v1/givings/offers", {
+      expect(api.get).toHaveBeenCalledWith("/api/v1/givings/offers_manager", {
         params: { params: { page: 0, per_page: 15 } },
       });
     });
