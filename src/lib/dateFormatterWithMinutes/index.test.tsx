ec13a43e3a09d40836e6d28f88b4d0bab7ea013f
let <let@MacBook-Air-de-let.local>
2022-11-23 13:13:09 -0300

    fix: date formatter with minutes test

--- src/lib/dateFormatterWithMinutes/index.test.tsx
+++ src/lib/dateFormatterWithMinutes/index.test.tsx
@@ -3,7 +3,7 @@ import dateFormatterWithMinutes from ".";
 describe("#dateFormatter", () => {
   it("returns correct date", () => {
     expect(dateFormatterWithMinutes("2022-06-20 17:55:49 UTC")).toBe(
-      "6/20/2022 5:55:49 PM",
+      "6/20/2022, 5:55:49 PM",
     );
   });
 });
