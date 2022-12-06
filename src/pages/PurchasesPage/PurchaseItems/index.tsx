25de3aa6a457837b93ba0364649460b97a37799a
thaisribon <thais@ribon.io>
2022-12-06 16:19:54 -0300

    feat: purchase value

--- src/pages/PurchasesPage/PurchaseItems/index.tsx
+++ src/pages/PurchasesPage/PurchaseItems/index.tsx
@@ -76,7 +76,8 @@ function PurchaseItems({ purchases, fetchPurchases, searchTerm }: Props) {
               ? purchase?.person?.guest?.walletAddress
               : purchase?.person?.customer?.email}
           </th>
-          <th>{purchase?.offer?.priceCents || "0"}</th>
+          <th>{purchase?.offer?.price || "0"}</th>
+          <th>{purchase?.cryptoAmount || "0"}</th>
           <th>
             <S.StatusTableCell
               style={{ color: statusColors[purchase?.status] }}
--- src/types/entities/PersonPayment.ts
+++ src/types/entities/PersonPayment.ts
@@ -4,6 +4,7 @@ import Person from "./Person";
 export default interface PersonPayment {
   id: number;
   amountCents: number;
+  cryptoAmount: number;
   paidDate: string;
   paymentMethod: string;
   status: string;
