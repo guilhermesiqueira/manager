cb7b57f836d9845642ea9f6034cb477fa7ad800a
Let <let@MacBook-Pro-de-Let.local>
2023-01-04 12:17:44 -0300

    fix: adjust tests

--- src/pages/integrations/IntegrationDetailsPage/index.test.tsx
+++ src/pages/integrations/IntegrationDetailsPage/index.test.tsx
@@ -8,8 +8,17 @@ describe("TreasureSection", () => {
     renderComponent(<IntegrationDetailsPage />);
 
     expect(screen.getByText("Status")).toBeInTheDocument();
+    expect(screen.getByText("ID")).toBeInTheDocument();
+    expect(screen.getByText("Name")).toBeInTheDocument();
+    expect(screen.getByText("Integration logo")).toBeInTheDocument();
     expect(screen.getByText("Wallet address")).toBeInTheDocument();
     expect(screen.getByText("Integration address")).toBeInTheDocument();
+    expect(
+      screen.getByText("Integration dashboard address"),
+    ).toBeInTheDocument();
+    expect(screen.getByText("Webhook Url")).toBeInTheDocument();
     expect(screen.getByText("Ticket availability")).toBeInTheDocument();
+    expect(screen.getByText("Created At")).toBeInTheDocument();
+    expect(screen.getByText("Last Edited At")).toBeInTheDocument();
   });
 });
