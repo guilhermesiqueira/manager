b9a83d68b0365fefd2b6f73fa2541602e29d1dd5
mendesmcmg <claraplanejamento@gmail.com>
2023-02-23 11:28:19 -0300

    test: tries to fix test

--- src/lib/dateFormatterWithMinutes/index.test.tsx
+++ src/lib/dateFormatterWithMinutes/index.test.tsx
@@ -2,7 +2,7 @@ import dateFormatterWithMinutes from ".";
 
 describe("#dateFormatter", () => {
   it("returns correct date", () => {
-    expect(dateFormatterWithMinutes("2022-06-20 17:55:49 UTC")).toBe(
+    expect(dateFormatterWithMinutes("2022-06-20 17:55:49 UTC").trim()).toBe(
       "6/20/2022, 5:55:49 PM",
     );
   });
