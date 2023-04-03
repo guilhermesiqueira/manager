aced1643ac78a75223c1dad09c0a1500eb916c01
mendesmcmg <claraplanejamento@gmail.com>
2023-04-03 11:52:51 -0300

    feat: adds update and get big donor requests

--- src/services/api/bigDonorsApi/index.ts
+++ src/services/api/bigDonorsApi/index.ts
@@ -1,10 +1,14 @@
 import { AxiosResponse } from "axios";
 import BigDonor from "types/entities/BigDonor";
-import { apiGet, apiPost } from "..";
+import { apiGet, apiPost, apiPut } from "..";
 
 const bigDonorsApi = {
   getBigDonorsList: (): Promise<AxiosResponse<BigDonor[]>> =>
     apiGet("big_donors"),
+  getBigDonor: (id: string | undefined): Promise<AxiosResponse<BigDonor>> =>
+    apiGet(`big_donors/${id}`),
+  updateBigDonor: (data: BigDonor): Promise<AxiosResponse<BigDonor>> =>
+    apiPut(`big_donors/${data.id}`, data),
   createBigDonor: (data: BigDonor): Promise<AxiosResponse<BigDonor>> =>
     apiPost("big_donors", data),
 };
