3b11a55c0c800dcc6ac5137b4ec0fcbd8b05274b
mendesmcmg <claraplanejamento@gmail.com>
2023-04-04 19:08:55 -0300

    feat: adds big donor search by name

--- src/pages/bigDonors/BigDonorsPage/BigDonorsItems/styles.ts
+++ src/pages/bigDonors/BigDonorsPage/BigDonorsItems/styles.ts
@@ -2,13 +2,11 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: flex;
-  flex-direction: column;
 `;
 
 // create a component to table
 export const Table = styled.table`
-  min-width: 700px;
-  max-width: 90%;
+  min-width: 500px;
   margin: ${({ theme }) => theme.spacing(24, 0)};
   border-collapse: collapse;
 
@@ -44,18 +42,4 @@ export const ActionsTableCell = styled.div`
 
 export const StatusTableCell = styled.span`
   text-transform: capitalize;
-`;
-
-export const ButtonContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-`;
-
-export const SearchBar = styled.input`
-  width: 80%;
-  height: 44px;
-  padding-left: ${({ theme }) => theme.spacing(16)};
-  border: 2px solid ${({ theme }) => theme.colors.neutral[500]};
-  border-radius: 8px;
-  background-color: ${({ theme }) => theme.colors.neutral[50]};
 `;
