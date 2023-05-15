5a7a820f7deb1f112c4930902b08a4c68fd2b784
mendesmcmg <claraplanejamento@gmail.com>
2023-05-15 09:00:16 -0300

    fix: offers request test

--- src/services/api/offersApi/index.test.ts
+++ src/services/api/offersApi/index.test.ts
@@ -10,12 +10,9 @@ describe("offersApi", () => {
     it("expects to send a get request with the correct info: url, params and headers", () => {
       offersApi.getOffersList({ perPage: 15, page: 0 });
 
-      expect(api.get).toHaveBeenCalledWith(
-        "/managers/v1/givings/offers_manager",
-        {
-          params: { params: { page: 0, per_page: 15 } },
-        },
-      );
+      expect(api.get).toHaveBeenCalledWith("/managers/v1/givings/offers", {
+        params: { params: { page: 0, per_page: 15 } },
+      });
     });
   });
 });
