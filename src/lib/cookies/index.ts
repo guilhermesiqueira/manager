9a3c2327f2ba52c1cbd7d18051676da98bbe9812
mendesmcmg <claraplanejamento@gmail.com>
2023-06-13 12:29:01 -0300

    fix: adds check for environment

--- src/lib/cookies/index.ts
+++ src/lib/cookies/index.ts
@@ -1,7 +1,13 @@
 import Cookies from "js-cookie";
 
+const isHttpsEnv = process.env.NODE_ENV !== "development";
+
 export function setCookiesItem(key: string, value: string): void {
-  Cookies.set(key, value, { secure: true, sameSite: "strict", httpOnly: true });
+  Cookies.set(key, value, {
+    secure: true,
+    sameSite: "strict",
+    httpOnly: isHttpsEnv,
+  });
 }
 
 export function getCookiesItem(key: string): string | null {
@@ -9,5 +15,9 @@ export function getCookiesItem(key: string): string | null {
 }
 
 export function removeCookiesItem(key: string) {
-  Cookies.remove(key, { secure: true, sameSite: "strict", httpOnly: true });
+  Cookies.remove(key, {
+    secure: true,
+    sameSite: "strict",
+    httpOnly: isHttpsEnv,
+  });
 }
