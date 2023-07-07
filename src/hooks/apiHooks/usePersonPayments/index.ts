1afb524e6109a9dc9f65a95ed3f92c5bacee5447
Karina Almeida <s.karinaalmeida@gmail.com>
2023-07-07 11:27:03 -0300

    fix: add page, per page and search params on person payments hook

--- src/hooks/apiHooks/usePersonPayments/index.ts
+++ src/hooks/apiHooks/usePersonPayments/index.ts
@@ -1,14 +1,32 @@
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import personPaymentsApi from "services/api/personPaymentsApi";
 import { CreateBigDonation } from "types/apiResponses/BigDonation";
 
 function usePersonPayments() {
+ 
+  const [perPage, setPerPage] = useState(10);
+  const [page, setPage] = useState(1);
+  const [searchTerm, setSearchTerm] = useState("");
   const getPersonPayments = useCallback(async () => {
+    
     const { data: allPersonPayments } =
-      await personPaymentsApi.getPersonPaymentsList();
-
+      await personPaymentsApi.getPersonPaymentsList({page, perPage: 10, searchTerm});
+      
     return allPersonPayments;
-  }, []);
+  }, [page, perPage, setPage, searchTerm]);
+
+  function incrementPage() {
+    setPage((oldPage) => oldPage + 1);
+  }
+
+  function updatePage(newPage: number) {
+    setPage(newPage);
+  }
+
+  function updateSearchTerm(newSearchTerm: string) {
+    setSearchTerm(newSearchTerm);
+  }
+
 
   async function createBigDonation(data: CreateBigDonation) {
     const bigDonation = personPaymentsApi.createBigDonation(data);
@@ -31,6 +49,11 @@ function usePersonPayments() {
   }
 
   return {
+    incrementPage,
+    updateSearchTerm,
+    updatePage,
+    setPerPage,
+    setPage,
     getPersonPayments,
     getBigDonorsPayments,
     createBigDonation,
