4a1456e252ad666d57ff47f4bac7e336835f95ba
Karina Almeida <s.karinaalmeida@gmail.com>
2023-07-11 09:32:26 -0300

    test: fix failing test

--- src/services/api/personPaymentsApi/index.test.ts
+++ src/services/api/personPaymentsApi/index.test.ts
@@ -9,10 +9,15 @@ describe("personPaymentsApi", () => {
 
     it("expects to send a get request with the correct info: url, params and headers", () => {
       const page = 1;
+      const perPage = 10;
       personPaymentsApi.getPersonPaymentsList({ page });
 
       expect(api.get).toHaveBeenCalledWith("/managers/v1/person_payments", {
-        params: {},
+        params: { params: {
+          page: page,
+          per_page: perPage,
+          search_term: ""
+        }},
       });
     });
   });
