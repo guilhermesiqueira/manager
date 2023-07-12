d71d02a849eb587e470a9b232769e205b9b4e032
Karina Almeida <s.karinaalmeida@gmail.com>
2023-07-12 12:19:14 -0300

    test: update test with status

--- src/services/api/personPaymentsApi/index.test.ts
+++ src/services/api/personPaymentsApi/index.test.ts
@@ -10,13 +10,15 @@ describe("personPaymentsApi", () => {
     it("expects to send a get request with the correct info: url, params and headers", () => {
       const page = 1;
       const perPage = 10;
+      const status = ["processing", "refunded", "paid", "failed"];
       personPaymentsApi.getPersonPaymentsList({ page });
 
       expect(api.get).toHaveBeenCalledWith("/managers/v1/person_payments", {
         params: { params: {
           page: page,
           per_page: perPage,
-          search_term: ""
+          search_term: "",
+          status: status,
         }},
       });
     });
