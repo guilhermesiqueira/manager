39cb42527328ff2bf54d880057bdbb462dbd1d4d
Henrique Barbosa <henrikleod@gmail.com>
2023-10-18 11:39:07 -0300

    feat: integrations api

---
+++ src/types/entities/ImpressionCard.ts
@@ -0,0 +1,10 @@
+export default interface ImpressionCard {
+  id: number;
+  headline: string;
+  title: string;
+  description: string;
+  videoUrl: string;
+  ctaText: string;
+  ctaUrl: string;
+  image: string;
+}
